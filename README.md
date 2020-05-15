6b66b2d4a89e0780b2cde5d08beabae72481d5f9
unknown <lavaman2013@gmail.com>
2020-05-14 22:30:10 -0500

    Added proper newline in the loop

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Prefixer
-Prefixer is a simple java program that adds a specified string to the beginning of each line
+Prefixer is a simple java program that adds a specified string to the beginning of each line.
+
+You can use this program by running it from the command line with the following arguments:
+"filename "Text to prefix with"
+
+an example run of the code will look like this:
+"java -jar Prefixer.jar "C:\users\user\Documents\example.txt" Prefixer
+
+
+Both arguments should be able to be passed without quotation marks unless you have whitespace in the file path or desired prefix string.
+
---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Made by horologium, the code is free to use, however.
+ * 
+ * @author horo
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("==========Prefixer Line Writer==========\n");
+		ArrayList<String> contents = new ArrayList<String>();
+		String line = "", fileArg = "", headerText = "";
+		if (args.length == 0) {
+			System.out.println("Please provide the proper arguments when running this file: \njava -jar example.txt \"Hello World\"\n\n");
+		}
+		fileArg = args[0];// File arg
+		headerText = args[1];// the string to insert
+		System.out.println("Each line will be prefixed by " + headerText + "\n");
+		BufferedReader fis = null;
+		try {
+			fis = new BufferedReader(new FileReader(fileArg));
+			System.out.println("Read file from " + fileArg +"\n");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Unable to read file, double check your file path and spelling.");
+		}
+		while ((line = fis.readLine()) != null && !line.equals("")) {
+			contents.add(line);
+		}
+		fis.close();
+		new FileWriter(fileArg, false).close();// Purges the file
+		fis = new BufferedReader(new FileReader(fileArg));//Reopens file in new instance
+		FileWriter f = new FileWriter(fileArg);
+		for (int i = 0; i < contents.size(); i++) {//Writes our text
+			f.write(headerText + " " + contents.get(i) + "\n");
+		}
+		System.out.println("All lines have the prefix applied. You can safely open the file.");
+		System.out.println("========================================\n");
+		f.close();
+		
+	}
+
+}
